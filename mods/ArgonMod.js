c161c670ed23ae94322cd6bf0f6ceb1381aa84fd
Ramen Guy <73126759+RamenGuy@users.noreply.github.com>
2024-01-28 13:01:04 -0600

    Update ArgonMod.js

--- mods/ArgonMod.js
+++ mods/ArgonMod.js
@@ -1,25 +1,4 @@
-function whenAvailable(names, callback) {
-    var interval = 10; // ms
-    window.setTimeout(function() {
-		let bool = true;
-		for(let i = 0; i < names.length; i++)
-		{
-			if(!window[names[i]])
-			{
-				bool = false;
-			}
-		}
-        if (bool) {
-            callback();
-        } else {
-            whenAvailable(names, callback);
-        }
-    }, interval);
-}
-var modName = "mods/ArgonMod.js";
-whenAvailable(["runAfterAutogen"], function() {
-
-    elements.argon = { 
+elements.argon = { 
         color: "#bababa",
         behavior: behaviors.GAS,
         colorOn: ["#ffaff8","#df88fc","#f842ff"],
@@ -29,9 +8,9 @@ whenAvailable(["runAfterAutogen"], function() {
         density: 0.9002,
         state: "gas",
         conduct: 0.86,
-    }
+};
     
-    elements.liquid_argon = {
+elements.liquid_argon = {
         color: "#d1d1d1",
         behavior: behaviors.LIQUID,
         colorOn: ["#ffaff8","#df88fc","#f842ff"],
@@ -44,5 +23,5 @@ whenAvailable(["runAfterAutogen"], function() {
         density: 1207,
         state: "liquid",
         conduct: 0.83,
-    }
-});
+};
+
