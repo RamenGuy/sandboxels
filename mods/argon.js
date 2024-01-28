b66ab35a8dc7e97d946ab9fa34fe213e60d68dea
Ramen Guy <73126759+RamenGuy@users.noreply.github.com>
2024-01-28 12:45:57 -0600

    Create argon.js

---
+++ mods/argon.js
@@ -0,0 +1,26 @@
+elements.argon = { 
+	color: "#bababa",
+	behavior: behaviors.GAS,
+	category: "gases",
+	density: 0.9002,
+	state: "gas",
+	tempLow: -246,
+	stateLow: "liquid_argon",
+	colorOn: ["#ffaff8","#df88fc","#f842ff"],
+	conduct: 0.86
+
+}
+elements.liquid_argon = {
+    color: "#d1d1d1",
+    behavior: behaviors.LIQUID,
+    colorOn: ["#ffaff8","#df88fc","#f842ff"],
+    temp: -247,
+    tempHigh: -246,
+    stateHigh: "argon",
+    tempLow: -248.6,
+    category: "states",
+    hidden: true,
+    density: 1207,
+    state: "liquid",
+    conduct: 0.83
+}
